fa494768231b7a4482dc6daf250d573463dd4678
shubham <shubhamsahu.sahu@gmail.com>
2025-07-04 14:15:40 +0530

    update the base URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://udsp.onrender.com/api';
 
 
 const api = axios.create({
