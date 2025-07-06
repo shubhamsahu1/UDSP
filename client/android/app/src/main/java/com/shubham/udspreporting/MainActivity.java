2d8e87ca68866a05c749b00ca436035c5174bf1e
shubham <shubhamsahu.sahu@gmail.com>
2025-07-06 12:37:36 +0530

    changes for android build

---
+++ client/android/app/src/main/java/com/shubham/udspreporting/MainActivity.java
@@ -0,0 +1,5 @@
+package com.shubham.udspreporting;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.shubham.udspreporting',
+  appName: 'UDSP Reporting',
+  webDir: 'build'
+};
+
+export default config;
